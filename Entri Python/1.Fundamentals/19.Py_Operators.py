f0283c624a37a2a5866f4e2c368cc9403831c2f6
Taroon Reddy <99872440+taroon-reddy@users.noreply.github.com>
2022-08-11 19:03:19 +0530

    Add files via upload

---
+++ Entri Python/1.Fundamentals/19.Py_Operators.py
@@ -0,0 +1,392 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ### Python Operators
+# Operators are used to perform operations on variables and values.
+
+# In[2]:
+
+
+1.2
+
+type(1.2)
+
+
+# In[3]:
+
+
+type(100)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# ### Arthimetic Operators
+
+# In[27]:
+
+
+x = 5
+y = 3
+
+#Addition
+print (x + y)
+
+
+# In[6]:
+
+
+#Subtraction
+print(x - y)
+
+
+# In[7]:
+
+
+x = 5
+y = 3
+
+#Multiplication
+print(x * y)
+
+
+# In[8]:
+
+
+x = 5
+y = 3
+
+#Division
+print(x / y)
+
+
+# In[9]:
+
+
+x = 5
+y = 3
+
+#Modulus
+print(x % y)
+
+
+# In[11]:
+
+
+x = 5
+y = 3
+
+# 5**3 = (5x5)5=125
+#Exponentiation
+print(x ** y)
+
+
+# In[12]:
+
+
+#Floor division
+print(x // y)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# ### Comparison Operators
+
+# In[13]:
+
+
+x = 5
+y = 3
+
+#Equal
+print(x == y)
+
+
+# In[14]:
+
+
+#Not equal
+print(x != y)
+
+
+# In[15]:
+
+
+#Greater than
+print(x > y)
+
+
+# In[16]:
+
+
+#Less than
+print(x < y)
+
+
+# In[17]:
+
+
+#Greater than or equal to
+print(x >= y)
+
+
+# In[18]:
+
+
+#Less than or equal to
+print(x <= y)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# ### Logical Operators
+
+# In[19]:
+
+
+#and - Returns True if both statements are true
+
+x < 5 and  x < 10
+
+#note x=5
+
+
+# In[20]:
+
+
+#or- Returns True if one of the statements is true
+x < 5 or x < 4
+
+
+# In[21]:
+
+
+#not - Reverse the result, returns False if the result is true
+not(x < 5 and x < 10)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# ### Identity Operators
+
+# In[26]:
+
+
+#is  - Returns true if both variables are the same object
+x is y
+
+#note x=5 and y =3
+
+
+# In[28]:
+
+
+#is not - Returns true if both variables are not the same object
+x is not y
+
+#note x=5 and y =3
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# ### Membership Operators
+
+# In[29]:
+
+
+#in - Returns True if a sequence with the specified value is present in the object
+
+x = ["Oranges", "Grapes"]
+
+print("Grapes" in x)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# ### Bitwise Operators
+
+# ###### & 	AND	Sets each bit to 1 if both bits are 1
+# ###### |	OR	Sets each bit to 1 if one of two bits is 1
+# ###### ^	XOR	Sets each bit to 1 if only one of two bits is 1
+# ###### ~ 	NOT	Inverts all the bits
+# ###### <<	Zero fill left shift	Shift left by pushing zeros in from the right and let the leftmost bits fall off
+# ###### >>	Signed right shift	Shift right by pushing copies of the leftmost bit in from the left, and let the rightmost bits fall off
+
+# In[ ]:
+
+
+
+
